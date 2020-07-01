416ec6d5e12ea189f0471f64c0056ffb967b26f5
deborahmancilha <deborahmancilha@outlook.com>
2020-07-01 13:14:04 -0300

    Versão 0.1

---
+++ uri1042/uri1042/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace ex1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            String[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+
+            if (a < b && b < c)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine("");
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+            }
+            else
+            {
+                if (a < c && c < b)
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(c);
+                    Console.WriteLine(b);
+                    Console.WriteLine("");
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    if (b < a && a < c)
+                    {
+                        Console.WriteLine(b);
+                        Console.WriteLine(a);
+                        Console.WriteLine(c);
+                        Console.WriteLine("");
+                        Console.WriteLine(a);
+                        Console.WriteLine(b);
+                        Console.WriteLine(c);
+                    }
+                    else
+                    {
+                        if (b < c && c < a)
+                        {
+                            Console.WriteLine(b);
+                            Console.WriteLine(c);
+                            Console.WriteLine(a);
+                            Console.WriteLine("");
+                            Console.WriteLine(a);
+                            Console.WriteLine(b);
+                            Console.WriteLine(c);
+                        }
+                        else
+                        {
+                            if (c < a && a < b)
+                            {
+                                Console.WriteLine(c);
+                                Console.WriteLine(a);
+                                Console.WriteLine(b);
+                                Console.WriteLine("");
+                                Console.WriteLine(a);
+                                Console.WriteLine(b);
+                                Console.WriteLine(c);
+                            }
+                            else
+                            {
+                                if (c < b && b < a)
+                                {
+                                    Console.WriteLine(c);
+                                    Console.WriteLine(b);
+                                    Console.WriteLine(a);
+                                    Console.WriteLine("");
+                                    Console.WriteLine(a);
+                                    Console.WriteLine(b);
+                                    Console.WriteLine(c);
+                                }
+                                else
+                                {
+
+                                    Console.WriteLine("");
+                                    Console.WriteLine(a);
+                                    Console.WriteLine(b);
+                                    Console.WriteLine(c);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
